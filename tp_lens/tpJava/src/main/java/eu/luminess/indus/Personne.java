53c6a0ccade40ee7cb777201908dc27708e144c4
DidierDefrance <didier@d-defrance.fr>
2023-03-13 16:51:34 +0100

    get/set

---
+++ tp_lens/tpJava/src/main/java/eu/luminess/indus/Personne.java
@@ -0,0 +1,80 @@
+package eu.luminess.indus;
+
+import java.util.Objects;
+//classe Java  avec attributs privés , get/set et contructeur par défaut = POJO = JavaBean
+public class Personne {
+    private  String nom;
+    private  Integer age; //null possible (et par défaut ici sur attribut , variable d'instance)
+    private Double poids;
+
+    public Personne(String nom, Integer age, Double poids) {
+        this.nom = nom;
+        this.age = age;
+        this.poids = poids;
+    }
+
+    public Personne() {
+        this(null,null,null);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        //this.age = age; //code généré par assistant
+        if(age <0 )
+            System.out.println("age négatif invalide"); //ou throw new RuntimeException("...");
+        else
+            this.age = age;
+    }
+
+    public Double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(Double poids) {
+        this.poids = poids;
+    }
+
+    public void incrementerAge(){
+        if(age != null)
+           this.age++; //age = age + 1;
+                       //age++;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "nom='" + nom + '\'' +
+                ", age=" + age +
+                ", poids=" + poids +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personne personne = (Personne) o;
+        return Objects.equals(nom, personne.nom) && Objects.equals(age, personne.age) && Objects.equals(poids, personne.poids);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, age, poids);
+    }
+
+    public void afficher(){
+        System.out.println(this.toString());
+    }
+}
