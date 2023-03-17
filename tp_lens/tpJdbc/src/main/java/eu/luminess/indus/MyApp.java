0cd876ca3b42f903cb648b19e2e2ee129fe5bff1
DidierDefrance <didier@d-defrance.fr>
2023-03-17 11:52:04 +0100

    s

--- tp_lens/tpJdbc/src/main/java/eu/luminess/indus/MyApp.java
+++ tp_lens/tpJdbc/src/main/java/eu/luminess/indus/MyApp.java
@@ -2,11 +2,13 @@ package eu.luminess.indus;
 
 import eu.luminess.indus.dao.DaoPersonne;
 import eu.luminess.indus.dao.DaoPersonneJdbc;
+import eu.luminess.indus.entity.Personne;
 
 public class MyApp {
 
     public static void main(String[] args) {
         DaoPersonne daoPersonne = new DaoPersonneJdbc();
+        daoPersonne.insert(new Personne("nouveau nom", 45, 77.7));
         daoPersonne.findAll().stream().forEach(p-> System.out.println(p));
         //...
     }
--- tp_lens/tpJdbc/src/main/java/eu/luminess/indus/dao/DaoPersonneJdbc.java
+++ tp_lens/tpJdbc/src/main/java/eu/luminess/indus/dao/DaoPersonneJdbc.java
@@ -2,9 +2,7 @@ package eu.luminess.indus.dao;
 
 import eu.luminess.indus.entity.Personne;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -32,24 +30,82 @@ public class DaoPersonneJdbc implements DaoPersonne{
     @Override
     public List<Personne> findAll() {
         List<Personne> personnes = new ArrayList<>();
-        try(Connection cn = this.getConnection()) {
-
+        try(Connection cn = this.getConnection();
+            Statement statement = cn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM personne")) {
+            while (rs.next()){
+                personnes.add(new Personne(
+                        rs.getInt("id"),
+                        rs.getString("nom"),
+                        rs.getInt("age"),
+                        rs.getDouble("poids")
+                ));
+            }
         }catch(SQLException ex){
-
+            ex.printStackTrace();
         }
         //Automatic close via try(withResource)
         //NB: dans projet plus élaborer cn.close() libère la connexion dans un pool de connexions
         return personnes;
     }
 
+    /*
+    public List<Personne> findAllV1() {
+        List<Personne> personnes = new ArrayList<>();
+        try(Connection cn = this.getConnection()) {
+            Statement statement = cn.createStatement();
+            String reqSql = "SELECT * FROM personne";
+            ResultSet rs = statement.executeQuery(reqSql);
+            while (rs.next()){
+                personnes.add(new Personne(
+                        rs.getInt("id"),
+                        rs.getString("nom"),
+                        rs.getInt("age"),
+                        rs.getDouble("poids")
+                ));
+            }
+            rs.close(); statement.close();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        //Automatic close via try(withResource)
+        //NB: dans projet plus élaborer cn.close() libère la connexion dans un pool de connexions
+        return personnes;
+    }
+    */
+
     @Override
     public Personne findById(int id) {
         return null;
     }
 
+    Integer recupValeurAutoIncrPk(PreparedStatement pst){
+        Integer pk=null;
+        try {
+            ResultSet rsKeys = pst.getGeneratedKeys();
+            if(rsKeys.next()){ pk= rsKeys.getInt(1); }
+        } catch (SQLException e) { e.printStackTrace(); }
+        return pk;
+    }
+
     @Override
     public Personne insert(Personne p) {
-        return null;
+        try(Connection cn = this.getConnection();
+            PreparedStatement pst = cn.prepareStatement("INSERT INTO personne(nom,age,poids) VALUES(?,?,?)",
+                                                          Statement.RETURN_GENERATED_KEYS ))
+        {
+            pst.setString(1, p.getNom());
+            pst.setInt(2, p.getAge());
+            pst.setDouble(3, p.getPoids());
+            pst.executeUpdate(); //avec auto_increment mysql sur colonne id
+            Integer pk = recupValeurAutoIncrPk(pst);
+            p.setId(pk);
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        //Automatic close via try(withResource)
+        //NB: dans projet plus élaborer cn.close() libère la connexion dans un pool de connexions
+        return p;
     }
 
     @Override
