f25be02ff1e9c898410bf3f6d50cafaacbe72602
didier <didier@d-defrance.fr>
2023-12-05 16:21:32 +0100

    avec test junit5

---
+++ tp_defense/projetJavaMaven/src/test/java/tp/TestPersonne.java
@@ -0,0 +1,20 @@
+package tp;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPersonne {
+	
+	
+	@Test
+	public void testIncrementerAge() {
+		Personne p1=new Personne("Bon" , "jean" , 42);
+		p1.incrementerAge();
+		System.out.println("nouvel age de p1=" + p1.getAge());
+		assertTrue(p1.getAge()==43);
+	}
+
+}
