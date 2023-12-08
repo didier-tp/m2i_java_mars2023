1d5193d204d9c3757e10f45007f9ad6720c190b3
didier <didier@d-defrance.fr>
2023-12-08 14:34:05 +0100

    s

--- tp_defense/projetJavaMaven/src/main/java/tp/thread/ComportementThread.java
+++ tp_defense/projetJavaMaven/src/main/java/tp/thread/ComportementThread.java
@@ -1,16 +1,24 @@
 package tp.thread;
 
 public class ComportementThread implements Runnable{
+	
+	private int compteur = 0;
 
 	@Override
 	public void run() {
 		for(int i=1 ; i<=4;i++) {
-			System.out.println("i="+i + " executé par " + Thread.currentThread().getName());
+			
+			synchronized(this) {
+				compteur=compteur+1;
+				System.out.println("i="+i + " compteur=" + compteur 
+						           + " executé par " + Thread.currentThread().getName());
+			}
 			try {
 				Thread.sleep(500);//500ms = 0.5s
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} 
+			
 		}
 		
 	}
