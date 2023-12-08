fcb7c7468078075c4f622f402334d0f8df81cfd3
didier <didier@d-defrance.fr>
2023-12-08 14:05:56 +0100

    s

--- tp_defense/projetJavaMaven/src/main/java/tp/thread/AppThread.java
+++ tp_defense/projetJavaMaven/src/main/java/tp/thread/AppThread.java
@@ -3,11 +3,13 @@ package tp.thread;
 public class AppThread {
 
 	public static void main(String[] args) {
-		System.out.println("debut-main");
-		ComportementThread c = new ComportementThread();
-		Thread t = new Thread(c);
-		t.start();
-		System.out.println("suite-main");
+		System.out.println("debut-main executé par " + Thread.currentThread().getName());
+		for(int i=1;i<=3;i++) {
+			ComportementThread c = new ComportementThread();
+			Thread t = new Thread(c);
+			t.start();
+		}
+		System.out.println("suite-main ");
 	}
 
 }
--- tp_defense/projetJavaMaven/src/main/java/tp/thread/ComportementThread.java
+++ tp_defense/projetJavaMaven/src/main/java/tp/thread/ComportementThread.java
@@ -4,8 +4,8 @@ public class ComportementThread implements Runnable{
 
 	@Override
 	public void run() {
-		for(int i=1 ; i<=10;i++) {
-			System.out.println("i="+i);
+		for(int i=1 ; i<=4;i++) {
+			System.out.println("i="+i + " executé par " + Thread.currentThread().getName());
 			try {
 				Thread.sleep(500);//500ms = 0.5s
 			} catch (InterruptedException e) {
