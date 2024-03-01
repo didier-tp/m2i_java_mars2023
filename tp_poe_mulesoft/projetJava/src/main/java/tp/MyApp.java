e2599a12c9fac4ded8df479c2f7dafab94dd0fa8
didier defrance <d2f.defrance@gmail.com>
2024-03-01 12:03:14 +0100

    debut_projetJava

---
+++ tp_poe_mulesoft/projetJava/src/main/java/tp/MyApp.java
@@ -0,0 +1,17 @@
+package tp;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");   
+        essai1();
+	}
+	
+	public static void essai1() {
+		int a=5;
+		int b=6;
+		int res=a+b;
+		System.out.println("res=" + res);   
+	}
+
+}
