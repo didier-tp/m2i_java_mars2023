cec31f57caa394c2fa5cc00493c4728a156017e6
didier defrance <d2f.defrance@gmail.com>
2024-03-01 14:07:02 +0100

    avec saisie

--- tp_poe_mulesoft/projetJava/src/main/java/tp/MyApp.java
+++ tp_poe_mulesoft/projetJava/src/main/java/tp/MyApp.java
@@ -1,10 +1,35 @@
 package tp;
 
+
+
 public class MyApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");   
-        essai1();
+        //essai1();
+		essai2();
+	}
+	
+	public static void essai2() {
+		//NB: .showInputDialog() affiche une boîte de dialogue qui récupére une valeur sous forme de String
+		//NB2 : Attention, la fintion JOptionPane.showInputDialog() n'est pas utilisable sur un ordinateur
+		//où il n'y a pas d'écran graphique.
+		//String sAge = javax.swing.JOptionPane.showInputDialog(null, "votre age");
+		
+		System.out.println("votre age:");
+		java.util.Scanner scanner = new java.util.Scanner(System.in); //pour une saisie au clavier en mode texte 
+		//(sans écran graphique , dans une console ou terminal texte)
+		String sAge = scanner.nextLine();
+		
+		System.out.println("sAge="+sAge);   
+		//nouvelAge=nouvelAge+1
+		/*
+		sAge=sAge+1; //concatenation plutot que addition
+		System.out.println("sAge="+sAge);
+		*/
+		int age = Integer.parseInt(sAge);
+		age=age+1;//addition
+		System.out.println("age incrémenté="+age); //le + entre 2 String déclenche une concatenation (chaines bout à bout )
 	}
 	
 	public static void essai1() {
@@ -13,6 +38,7 @@ public class MyApp {
 		int b=10;
 		int res=a/b; 
 		*/
+	
 		double a=36.0;
 		double b=10.0;
 		double res=a/b; 
