52eb0e922f8815b203270e3a82691885da557d45
didier defrance <d2f.defrance@gmail.com>
2024-03-01 13:18:46 +0100

    s

--- tp_poe_mulesoft/projetJava/src/main/java/tp/MyApp.java
+++ tp_poe_mulesoft/projetJava/src/main/java/tp/MyApp.java
@@ -8,10 +8,22 @@ public class MyApp {
 	}
 	
 	public static void essai1() {
-		int a=5;
-		int b=6;
-		int res=a+b;
-		System.out.println("res=" + res);   
+		/*
+		int a=36;
+		int b=10;
+		int res=a/b; 
+		*/
+		double a=36.0;
+		double b=10.0;
+		double res=a/b; 
+		
+		System.out.println("res=" + res);  
+		if(res >=13)
+			System.out.println("res est superieur ou egal à 13");
+		else 
+			System.out.println("res est plus petit que 13");
+		boolean vraiOuFaux = (res >=13);
+		System.out.println("vraiOuFaux=" + vraiOuFaux);  
 	}
 
 }
