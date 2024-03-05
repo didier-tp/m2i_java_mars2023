c2ce1dea46abb1661af25297084fd4e6ba78b737
didier defrance <d2f.defrance@gmail.com>
2024-03-05 15:17:19 +0100

    sciptLancement

---
+++ tp_poe_mulesoft/projetJavaDidier/lancerAppliViaJarEtJdk.bat
@@ -0,0 +1,8 @@
+set JAVA_HOME=C:\Program Files\java\jdk-17
+REM set JAVA_HOME=C:\Program Files\eclipse\plugins\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_17.0.2.v20220201-1208\jre
+set PATH=%JAVA_HOME%\bin
+cd "%~dp0"
+cd target
+java -classpath projetJavaDidier.jar tp.MyApp
+REM java -jar myAppli.jar
+pause
