99640b8795955229288c66ed231866fa7aee7947
DidierDefrance <didier@d-defrance.fr>
2024-03-06 16:44:24 +0100

    s

--- tp_poe_mulesoft/projetJava2Didier/src/main/java/tp/MyApp2.java
+++ tp_poe_mulesoft/projetJava2Didier/src/main/java/tp/MyApp2.java
@@ -9,6 +9,7 @@ import java.util.Map;
 public class MyApp2 {
 
 	public static void main(String[] args) {
+		System.out.println("os.name=" + System.getProperty("os.name"));
 		//essaiAnimaux();
 		//essaiException();
 		//essaiCollection();
