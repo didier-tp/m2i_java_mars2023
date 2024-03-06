0f592f48dd06ddf494ae720c81e88d1b957d0801
DidierDefrance <didier@d-defrance.fr>
2024-03-06 17:03:36 +0100

    s

--- tp_poe_mulesoft/projetJava2Didier/src/main/java/tp/MyApp3.java
+++ tp_poe_mulesoft/projetJava2Didier/src/main/java/tp/MyApp3.java
@@ -4,14 +4,18 @@ public class MyApp3 {
 
 	public static void main(String[] args) {
 		//lancement de MyApp3:
-		//java -Daff.maj=true  tp.MyApp3 9 message1
+		//java ... -Daff.maj=true  tp.MyApp3 9 message1 (dans .bat de lancement)
+		//ou bien dans onglet "arguments" de "run configuration" de eclipse
+		//  9 message dans "program argument"
+		// -Daff.maj=true dans VM arguments
+		
 		//resultat:  racineCarree=3 message=MESSAGE1
 		double valeur = Double.parseDouble(args[0]);
 		String message=args[1];
 		boolean affMaj = Boolean.parseBoolean(System.getProperty("aff.maj"));
 		double racineCarre= Math.sqrt(valeur);
 		if(affMaj) message=message.toUpperCase();
-		System.out.println("racineCarre="+racineCarre + " message="+racineCarre);
+		System.out.println("racineCarre="+racineCarre + " message="+message);
 		
 	}
 
