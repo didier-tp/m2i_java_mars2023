c8b76d56735899f456a696c580983684420a2f0f
DidierDefrance <didier@d-defrance.fr>
2024-03-07 15:17:31 +0100

    s

--- tp_poe_mulesoft/projetFichiers/src/main/java/tp/Produit.java
+++ tp_poe_mulesoft/projetFichiers/src/main/java/tp/Produit.java
@@ -1,8 +1,5 @@
 package tp;
 
-import jakarta.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "produit")
 public class Produit {
    //à coder avec numero,label,prix
 	private int numero;
--- tp_poe_mulesoft/projetFichiers/src/main/java/tp/Produits.java
+++ tp_poe_mulesoft/projetFichiers/src/main/java/tp/Produits.java
@@ -9,11 +9,11 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "produits")
-@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "produits")   //nom de la balise (racine, de premier niveau)
+@XmlAccessorType(XmlAccessType.FIELD) //pour tenir compte de @XmlElement sur le "field" en private
 public class Produits {
 	
-	@XmlElement(name="produit")
+	@XmlElement(name="produit") //nom de la sous-balise pour chaque element de la liste
     private List<Produit> listeProduits =null;
 
 	public List<Produit> getListeProduits() {
