fcc6a88ac6f4fa4f0f56f12ad5c35c1b66880974
DidierDefrance <didier@d-defrance.fr>
2024-03-07 10:05:20 +0100

    s

--- tp_poe_mulesoft/projetFichiers/src/main/java/tp/MyApp.java
+++ tp_poe_mulesoft/projetFichiers/src/main/java/tp/MyApp.java
@@ -1,5 +1,13 @@
 package tp;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyApp {
 
 	public static void main(String[] args) {
@@ -8,7 +16,20 @@ public class MyApp {
 	}
 	
 	public static void essaiLecture() {
-		
+		List<String> listeLignes = new ArrayList<>();
+		try {
+			FileInputStream fis = new FileInputStream("data.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			String ligne = br.readLine();
+			while(ligne!=null) {
+				listeLignes.add(ligne);
+				ligne = br.readLine(); //on tente de lire la ligne suivante
+			}
+			br.close(); fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("listeLignes="+listeLignes);
 	}
 	
 	
