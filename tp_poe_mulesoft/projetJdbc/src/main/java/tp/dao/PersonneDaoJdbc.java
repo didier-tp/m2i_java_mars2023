2395b253c7cce808c97bff6028097055437c1570
didier defrance <d2f.defrance@gmail.com>
2024-03-08 12:01:56 +0100

    s

---
+++ tp_poe_mulesoft/projetJdbc/src/main/java/tp/dao/PersonneDaoJdbc.java
@@ -0,0 +1,39 @@
+package tp.dao;
+
+import java.util.List;
+
+import tp.data.Personne;
+
+public class PersonneDaoJdbc implements PersonneDao {
+
+	@Override
+	public Personne saveNew(Personne p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personne getPersonneById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Personne> getAllPersonnes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updatePersonne(Personne p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePersonne(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
