186ee105a13731816d10114642025e8430ddb2bf
didier defrance <d2f.defrance@gmail.com>
2024-03-08 11:16:30 +0100

    s

---
+++ tp_poe_mulesoft/projetJdbc/creer_base.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS mydb;
+USE mydb;
+
+DROP TABLE IF EXISTS personne;
+
+
+CREATE TABLE personne(
+    id integer auto_increment,
+	nom VARCHAR(64),
+	age integer,
+	poids double,
+	PRIMARY KEY(id));
+
+#########################  INSERT INTO   #####################################
+
+INSERT INTO personne(id,nom,age,poids) VALUES (1,'jean_bon',45, 67.8);
+INSERT INTO personne(id,nom,age,poids) VALUES (2,'axelle_aire',38, 82.56);
+ 
+###################### VERIFICATIONS ###########################################
+show tables;
+SELECT * FROM personne;
+	
