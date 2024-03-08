37f934ce27384c544fecef31abbcff341cfca134
didier defrance <d2f.defrance@gmail.com>
2024-03-08 10:35:00 +0100

    s

--- tp_poe_mulesoft/projetJdbc/src/main/java/tp/MyApp.java
+++ tp_poe_mulesoft/projetJdbc/src/main/java/tp/MyApp.java
@@ -1,10 +1,18 @@
 package tp;
 
+import java.sql.Connection;
+
 public class MyApp {
 
 	public static void main(String[] args) {
-	
+	     essaiConnexion();
 
+	}
+	
+	public static void essaiConnexion() {
+		ConnexionUtil connexionUtil =new ConnexionUtil();
+		Connection cn = connexionUtil.etablirConnexion();
+		System.out.println("cn="+cn);
 	}
 
 }
