e2ab2fbe9c921a07abbfc773e746320aa4bcccfe
didier defrance <d2f.defrance@gmail.com>
2024-03-08 12:29:56 +0100

    s

--- tp_poe_mulesoft/projetJdbc/src/main/java/tp/MyApp.java
+++ tp_poe_mulesoft/projetJdbc/src/main/java/tp/MyApp.java
@@ -1,12 +1,23 @@
 package tp;
 
 import java.sql.Connection;
+import java.util.List;
+
+import tp.dao.PersonneDao;
+import tp.dao.PersonneDaoJdbc;
+import tp.data.Personne;
 
 public class MyApp {
 
 	public static void main(String[] args) {
-	     essaiConnexion();
-
+	     //essaiConnexion();
+		essaiGetAllPersonnes();
+	}
+	
+	public static void essaiGetAllPersonnes() {
+		PersonneDao personneDao = new PersonneDaoJdbc();
+		List<Personne> personnes = personneDao.getAllPersonnes();
+		System.out.println("personnes="+personnes);
 	}
 	
 	public static void essaiConnexion() {
