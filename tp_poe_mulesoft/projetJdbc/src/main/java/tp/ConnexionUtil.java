ab82c2b4dc5fb63c63dfbd8a67c18fedf0379c9d
didier defrance <d2f.defrance@gmail.com>
2024-03-08 09:47:11 +0100

    s

---
+++ tp_poe_mulesoft/projetJdbc/src/main/java/tp/ConnexionUtil.java
@@ -0,0 +1,28 @@
+package tp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnexionUtil {
+	
+	public Connection etablirConnexion() {
+		Connection cn =null;
+		ResourceBundle ressources = ResourceBundle.getBundle("paramDB") ; // paramDB.properties
+		String driver = ressources.getString("driver");
+		String chUrl = ressources.getString("url");
+		String username = ressources.getString("username");
+		String password = ressources.getString("password");
+		try {
+			Class.forName(driver); 
+			cn = DriverManager.getConnection(chUrl,username,password) ;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return cn;
+	}
+
+}
