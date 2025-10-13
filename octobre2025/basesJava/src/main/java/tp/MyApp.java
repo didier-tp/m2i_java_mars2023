40f98a7bc5cc99b353ddc879d2b2d7587a47fb3d
Didier Defrance <didier@d-defrance.fr>
2025-10-13 17:13:55 +0200

    s

--- octobre2025/basesJava/src/main/java/tp/MyApp.java
+++ octobre2025/basesJava/src/main/java/tp/MyApp.java
@@ -34,11 +34,23 @@ public class MyApp {
 	  public static void testerPersonne() {
 		  Personne p1=null;
 		  p1= new Personne();
-		  p1.nom="toto";  p1.age=44;  p1.poids=66.8;
+		  p1.setNom("toto"); p1.setAge(44); p1.setPoids(66.0);
+		  //p1.nom="toto";  p1.age=44;  p1.poids=66.0; //seulement possible dans v1 où tout est public
 		  p1.afficher();  //.afficher() appele en interne System.out.println(...)
-		  Personne p2= new Personne();
-		  p2.nom="titi";  p2.age=34;  p2.poids=77.8;
+		  System.out.println("p1=" + p1.toString() );
+		  Personne p2= new Personne("titi",34,77.8);
+		  //p2.nom="titi";  p2.age=34;  p2.poids=77.8; //seulement possible dans v1 où tout est public
 		  p2.afficher(); 
+		  System.out.println("p2=" + p2 ); //sous entendu  + p2.toString()
+		  Personne p3= new Personne("titi",34,77.8);
+		  if(p3.equals(p2))  //NB : il faut générer .equals() sur la classe Personne par assistant eclipse ou autre
+			  System.out.println("p2 et p3 ont les mêmes valeurs internes");
+		  else
+		       System.out.println("p2 et p3 n'ont pas les mêmes valeurs internes");
+		  p2.setAge(-23); //message d'erreur (age invalide)
+		  System.out.println("p2=" + p2 ); //age inchangé
+		  p2.setAge(23);
+		  System.out.println("p2=" + p2 ); //nouvel age maintenant 23
 	  }
 	
       public static void main(String[] args) {
--- octobre2025/basesJava/src/main/java/tp/Personne.java
+++ octobre2025/basesJava/src/main/java/tp/Personne.java
@@ -1,14 +1,88 @@
 package tp;
 
+import java.util.Objects;
+
 public class Personne {
 	
-	public String nom;
-	public Integer age;
-	public Double poids;
+	private String nom;
+	private Integer age;
+	private Double poids;
 	
-	
-	public void afficher(){
-		System.out.println("Personne avec nom=" + this.nom +  " et age=" + this.age + " et poids=" + this.poids);
+
+	public Personne(String nom, Integer age, Double poids) {
+		super();
+		this.nom = nom;
+		this.age = age;
+		this.poids = poids;
 	}
+	
+    //constructeur par défaut (avec zéro paramètre)
+	public Personne() {
+		super();
+	}
+
+
+	public void afficher(){
+		//System.out.println("Personne avec nom=" + this.nom +  " et age=" + this.age + " et poids=" + this.poids);
+		System.out.println(this.toString());
+	}
+
+
+	@Override
+	public String toString() {
+		return "Personne [nom=" + nom + ", age=" + age + ", poids=" + poids + "]";
+	}
+
+	//get..., set...() générés par assistants de eclipse ou intelliJ:
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		//this.age = age;// code généré par assistant
+		if(age>=0)
+			this.age = age;
+		else {
+			System.out.println("age négatif invalide");
+			//throw new RuntimeException("age négatif invalide")
+		}
+	}
+
+	public Double getPoids() {
+		return poids;
+	}
+
+	public void setPoids(Double poids) {
+		this.poids = poids;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, nom, poids);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		return Objects.equals(age, other.age) && Objects.equals(nom, other.nom) && Objects.equals(poids, other.poids);
+	}
+	
+	
+	
 
 }
