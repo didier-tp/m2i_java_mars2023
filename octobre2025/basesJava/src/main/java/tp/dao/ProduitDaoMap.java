e23ced6f9d1d48d39b843f24f6ef439bc14b716c
Didier Defrance <didier@d-defrance.fr>
2025-10-15 17:23:00 +0200

    s

---
+++ octobre2025/basesJava/src/main/java/tp/dao/ProduitDaoMap.java
@@ -0,0 +1,45 @@
+package tp.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import tp.Produit;
+
+public class ProduitDaoMap implements ProduitDAO {
+	
+	private int lastNum=0;
+	private Map<Integer,Produit> mapNumeroProduit = new HashMap<>(); 
+
+	@Override
+	public Produit createProduit(Produit p) {
+		this.lastNum++; //simuler auto_incr
+		p.setNumero(this.lastNum);
+		this.mapNumeroProduit.put(p.getNumero(), p);
+		return p; //retourne le produit avec le numero
+	}
+
+	@Override
+	public Produit findById(int numero) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Produit> findAll() {
+		return ( List<Produit>) this.mapNumeroProduit.values();
+	}
+
+	@Override
+	public void updateProduit(Produit p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int numero) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
