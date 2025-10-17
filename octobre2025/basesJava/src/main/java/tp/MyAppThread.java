aa15ecfcb6673f9b194bd3f25d9bd8b5d469a4c5
Didier Defrance <didier@d-defrance.fr>
2025-10-17 10:20:42 +0200

    s

--- octobre2025/basesJava/src/main/java/tp/MyAppThread.java
+++ octobre2025/basesJava/src/main/java/tp/MyAppThread.java
@@ -4,11 +4,14 @@ public class MyAppThread {
 	
 
 	public static void main(String[] args) {
+		MyThreadUtil.afficherMessage("debut de main");
+		
 		TacheCafe tacheCafe = new TacheCafe(); //code qui sera exécuté par un ou plusieur thread
 		
 		Thread thread_a = new Thread(tacheCafe); //préparer un nouveau Thread qui va executer la tâche (en // vis à vis du Thread principal)
 		
 		thread_a.start(); //démarrer le nouveau thread , celui ci s'arrêtera tout seul quand le .run() aura fini son travail
+		MyThreadUtil.afficherMessage("suite du main");
 	}
 	
 }
--- octobre2025/basesJava/src/main/java/tp/TacheCafe.java
+++ octobre2025/basesJava/src/main/java/tp/TacheCafe.java
@@ -1,12 +1,22 @@
 package tp;
 
 public class TacheCafe implements Runnable {
+	
+	private boolean estLong =false;
+	
+	TacheCafe(boolean estLong){
+		this.estLong=estLong;
+	}
+	
+	TacheCafe(){
+		this(false);
+	}
 
 	@Override
 	public void run() {
 		MyThreadUtil.afficherMessage("café en préparation");
 		try {
-			Thread.sleep(5000); //5000ms de pause
+			Thread.sleep(this.estLong?3000:1000); //3000ms de pause pour cefe long, 1000 pour café court
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
