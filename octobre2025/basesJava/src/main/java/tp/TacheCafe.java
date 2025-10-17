f398d6bd01d1356568642c26f9706928ecf38107
Didier Defrance <didier@d-defrance.fr>
2025-10-17 10:41:48 +0200

    s

--- octobre2025/basesJava/src/main/java/tp/TacheCafe.java
+++ octobre2025/basesJava/src/main/java/tp/TacheCafe.java
@@ -3,6 +3,7 @@ package tp;
 public class TacheCafe implements Runnable {
 	
 	private boolean estLong =false;
+	private int compteur=0;
 	
 	TacheCafe(boolean estLong){
 		this.estLong=estLong;
@@ -15,6 +16,15 @@ public class TacheCafe implements Runnable {
 	@Override
 	public void run() {
 		MyThreadUtil.afficherMessage("café en préparation");
+		synchronized(this) {
+		    this.compteur++;
+		    MyThreadUtil.afficherMessage("compteur="+this.compteur + " avec estLong=" + this.estLong);
+		}
+		
+		//NB: PAS BESOIN de synchronized si manipulation de variables locales (ex: i  ou input ou en return)
+		for(int i=0;i<5;i++) {
+			System.out.print(" " + i); 
+		}
 		try {
 			Thread.sleep(this.estLong?3000:1000); //3000ms de pause pour cefe long, 1000 pour café court
 		} catch (InterruptedException e) {
