7816b6548ddfa1657a9885bbe64e3c87bc289cfc
Didier Defrance <didier@d-defrance.fr>
2025-10-17 10:07:41 +0200

    s

--- octobre2025/basesJava/src/main/java/tp/TacheCafe.java
+++ octobre2025/basesJava/src/main/java/tp/TacheCafe.java
@@ -4,13 +4,13 @@ public class TacheCafe implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println("café en préparation");
+		MyThreadUtil.afficherMessage("café en préparation");
 		try {
 			Thread.sleep(5000); //5000ms de pause
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println("café prêt");
+		MyThreadUtil.afficherMessage("café prêt");
 	}
 
 }
