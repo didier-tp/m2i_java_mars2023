e6f3b01960f8971ef5f3da48e9cd5876e173f015
Didier Defrance <didier@d-defrance.fr>
2025-10-17 11:58:22 +0200

    s

--- octobre2025/scripts_mysql/creer_base.bat
+++ octobre2025/scripts_mysql/creer_base.bat
@@ -1,6 +1,6 @@
 REM lancer si besoin le service mysql (net start mysql ou mariadb)
 REM set MYSQL_HOME=C:\Prog\DB\MySql5
-set MYSQL_HOME=C:\Program Files\MariaDB 11.2
+set MYSQL_HOME=C:\Program Files\MariaDB 11.8
 cd /d %~dp0
 REM mot de passe souvent root ou formation ou rien
 "%MYSQL_HOME%\bin\mysql"  -u root -p < create_db.sql
